bafa57945f7ca3103dc6acac4110b4ce86146332
takehiro-yamamoto <xj543ade@gmail.com>
2025-05-31 23:36:32 +0900

    バリデーション追加

--- app/models/completion_log.rb
+++ app/models/completion_log.rb
@@ -2,5 +2,6 @@ class CompletionLog < ApplicationRecord
   belongs_to :chore
   belongs_to :user
 
-  validates :completed_at, presence: true
+  validates :completed_at, presence: true # 完了日時は必須
+  validates :note, length: { maximum: 100 } # 100文字以内入力可能にする
 end
