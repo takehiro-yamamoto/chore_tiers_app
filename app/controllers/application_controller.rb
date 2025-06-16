1acb0f745abbbb7f0f21955dd757d5167b5ecba4
takehiro-yamamoto <xj543ade@gmail.com>
2025-06-16 12:06:50 +0900

    Basic認証設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,4 +33,10 @@ private
     store_location_for(:user, request.fullpath)
   end
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
+
 end
