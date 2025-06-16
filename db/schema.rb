1dd3ee363a8965a2b6b69b32fc6e81323ee447b4
takehiro-yamamoto <xj543ade@gmail.com>
2025-06-16 16:45:46 +0900

    Fix: Set email limit to 191 to avoid MySQL index length error

--- db/schema.rb
+++ db/schema.rb
@@ -109,7 +109,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_14_130951) do
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", limit: 191, default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
