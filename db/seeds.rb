c28084876609a357971763589e1d1a4b6739b10e
takehiro-yamamoto <xj543ade@gmail.com>
2025-06-16 19:35:22 +0900

    ティアリストDBのseedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+tiers = [
+  { name: '最高', label: 'S', priority: 1 },
+  { name: '高', label: 'A', priority: 2 },
+  { name: '中', label: 'B', priority: 3 },
+  { name: '低', label: 'C', priority: 4 },
+  { name: '最低', label: 'D', priority: 5 }
+]
+
+tiers.each do |tier|
+  Tier.find_or_create_by!(name: tier[:name]) do |t|
+    t.label = tier[:label]   # CSSクラス名に一致させる
+    t.priority = tier[:priority]
+  end
+end
